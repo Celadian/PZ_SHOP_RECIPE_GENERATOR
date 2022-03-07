ad2bd399de1d4b533dad12eb1136a06048c6d967
Celadian <thomas.carroll67@gmail.com>
2022-03-06 20:02:47 -0600

    Setting it up for a trader. You can now specify what items you want to keep. Items are now also usable from the floor.

---
+++ RecipeGenerator/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+U
+%src/main/java/Application/Recipe.java,0\5\05247f647bfc3de6e312099edb6cefe55a472f90
+Z
+*src/main/java/Application/Application.java,4\5\45295b7fa270bb3db61549e2132010f1622dc7de
+^
+.src/main/java/Application/RecipeGenerator.java,f\8\f8aa5e808eb913d258a597ee672624f100d8bbb3
---
+++ RecipeGenerator/src/main/java/Application/Recipe.java
@@ -0,0 +1,20 @@
+package Application;
+
+public class Recipe{
+    String txnType;
+    String identifier;
+    String dispName;
+    String money;
+    String itemName;
+    String time;
+
+
+    public Recipe(String txnType,String identifier, String dispName, String itemName, String money, String time) {
+        this.txnType = txnType;
+        this.identifier = identifier;
+        this.dispName = dispName;
+        this.money = money;
+        this.itemName = itemName;
+        this.time = time;
+    }
+}
